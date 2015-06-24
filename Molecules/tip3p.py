97b62c52dc92a28994cfde56bbe349b6451bc082
vitroid <vitroid@gmail.com>
2015-06-24 23:03:04 +0900

    Add TIP5P.

--- Molecules/tip3p.py
+++ Molecules/tip3p.py
@@ -7,11 +7,11 @@ hangle = 104.52 * math.pi / 180 / 2
 mass=18
 ohz = oh * math.cos(hangle)
 ohy = oh * math.sin(hangle)
+oz  = -ohz*2/mass
 
 
-
-sites = np.array([[0, 0,-ohz*2/mass],
-                  [0, ohy,ohz*16/mass],
-                  [0,-ohy,ohz*16/mass]]) # nm, OHHM
+sites = np.array([[0, 0,oz],
+                  [0, ohy,ohz+oz],
+                  [0,-ohy,ohz+oz]]) # nm, OHHM
 
 labels = ["OW","HW1","HW2"]
