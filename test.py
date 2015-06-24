a76330fefc5dad2decbc2c8e2353fa65d8d91f74
vitroid <vitroid@gmail.com>
2015-06-25 00:59:50 +0900

    Make some ice unit structures larger.
    Make some ice unit structures larger in order to guarantee the
    generation of 1x1x1 lattice.

---
+++ test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from subprocess import check_output
+import sys
+
+ices = "0 i 1c 1h 3 4 5 6 7 12 16 CS1 TS1 HS1 C0-II sTprime".split()
+
+if len(sys.argv) > 1:
+    mode = sys.argv[1]
+
+if mode == "2":
+    for ice in ices:
+        print("Ice",ice)
+        result = check_output(["./genice", "-r","1","1","1", ice]).decode('utf-8')
+        lines = result.split("\n")
+        print(lines[-10:-1])
+    sys.exit(0)
+    
+
+
+for ice in ices:
+    for f in "mgdeq":
+        result = check_output(["./genice", "-f", f, ice])
+        lines = result.split("\n")
+        print(lines[-10:-1])
+sys.exit(0)
