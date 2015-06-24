c7047a5e22ff25d397514e3f12b7891c05ef8d8a
vitroid <vitroid@gmail.com>
2015-06-25 01:04:16 +0900

    Make pair list before replication.

--- test.py
+++ test.py
@@ -5,6 +5,7 @@ import sys
 
 ices = "0 i 1c 1h 3 4 5 6 7 12 16 CS1 TS1 HS1 C0-II sTprime".split()
 
+mode = "1"
 if len(sys.argv) > 1:
     mode = sys.argv[1]
 
@@ -20,7 +21,7 @@ if mode == "2":
 
 for ice in ices:
     for f in "mgdeq":
-        result = check_output(["./genice", "-f", f, ice])
+        result = check_output(["./genice", "-f", f, ice]).decode('utf-8')
         lines = result.split("\n")
         print(lines[-10:-1])
 sys.exit(0)
