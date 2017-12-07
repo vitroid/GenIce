207c870f0fd8d77d6c29f12f91de75dd4c2e9a70
Masakazu Matsumoto <vitroid@gmail.com>
2017-12-07 19:46:41 +0900

    Changed the description of the alkyl trees.
    Orientations of the first arm from the nitrogen are optimised.

--- genice/alkyl.py
+++ genice/alkyl.py
@@ -6,59 +6,85 @@ import logging
 from math import pi, sin, cos
 
 #a sample for general alkyl group with methyls
-# 3-methylbutyl : backbone=[["Ma",[],[]],["Mb",[],[]],["Mc",[["Me",[],[]]],[]],["Md",[],[]]]
+# tree | branch: [root,branch1,branch2,branch3]
+# or             [root]
+# or             leaf
+# 3-methylbutyl : backbone=["Ma",["Mb",["Mc",["Md","Me"]]]]]
 #v1 and v2 must be given as a unit vector.
-def alkyl(v1, v2, backbone):
+def alkyl(direction, destination, tree, dest=None):
     """
     put a normal-alkyl group rooted at root toward cpos.
     """
     logger = logging.getLogger()
+    logger.debug("  {0}".format(tree))
     # logger.info("  Put butyl at {0}".format(molname))
+
+    if type(tree) is not list:
+        tree = [tree]
+
+    # v2 is a vector from direction to the destination
+    v2 = destination - direction
+    v2 /= np.linalg.norm(v2)
+    v2d = np.dot(direction, v2)
+    while v2d > 0.999:
+        #They are inline. It is not safe to determine the orientation.
+        v2 = np.random.random(3)
+        v2 /= np.linalg.norm(v2)
+        v2d = np.dot(direction, v2)
+
+    v2 -= v2d * direction
+    v2 /= np.linalg.norm(v2)
+
+    #v1 is the pivot
+    v1 = direction
+    
     v3 = np.cross(v1, v2)     # the thild unit vector
 
-    c = cos(109.5 / 2 * pi / 180)
-    s = (1.0 - c**2)**0.5
+    c = cos(120 * pi / 180)
+    s = sin(120 * pi / 180)
     v4 = v2*c + v3*s   # a branch vector
     v5 = v2*c - v3*s   # another branch vector
+    logger.debug("  {0} -0.5".format(np.dot(v2, v4)))
+    logger.debug("  {0} -0.5".format(np.dot(v4, v5)))
+    logger.debug("  {0} -0.5".format(np.dot(v5, v2)))
 
-    logger.debug("  {0} {1} {2}".format(
-        np.dot(v1, v1), np.dot(v2, v2), np.dot(v1, v2)))
+    c = cos(109.5 * pi / 180)
+    s = sin(109.5 * pi / 180)
+    v2 = -v1*c + v2*s
+    v4 = -v1*c + v4*s
+    v5 = -v1*c + v5*s
+    logger.debug("  {0} 1/3".format(np.dot(v1, v2)))
+    logger.debug("  {0} 1/3".format(np.dot(v1, v4)))
+    logger.debug("  {0} 1/3".format(np.dot(v1, v5)))
+    logger.debug("  {0} 1".format(np.dot(v2, v2)))
+    logger.debug("  {0} 1".format(np.dot(v4, v4)))
+    logger.debug("  {0} 1".format(np.dot(v5, v5)))
 
-    atoms = []
-    for i, meth in enumerate(backbone):
-        center, b1, b2 = meth
-        x = s * (i + 1)
-        y = ((i + 1) % 2) * c
-        cpos = x * v1 + y * v2
-        atoms.append([center, cpos])
-
-        if len(b1) > 0:
-            newv1 = (v4-v5)/2
-            newv2 = v4 - newv1
-            newv1 /= np.linalg.norm(newv1)
-            newv2 /= np.linalg.norm(newv2)
-            adatoms = alkyl(newv1, newv2, b1)
-            for atom in adatoms:
-                atoms.append((atom[0],atom[1]+cpos))
-        if len(b2) > 0:
-            newv1 = (v5-v4)/2
-            newv2 = v5 - newv1
-            newv1 /= np.linalg.norm(newv1)
-            newv2 /= np.linalg.norm(newv2)
-            adatoms = alkyl(newv1, newv2, b2)
-            for atom in adatoms:
-                atoms.append((atom[0],atom[1]+cpos))
-        #Alternated
-        v4 = -v4
-        v5 = -v5
+    
+    #assert False
+    atomname = tree[0]
+    atoms = [(atomname, np.zeros(3))]
+    if len(tree) > 1:
+        atoms += alkyl(v2, destination - v1, tree[1]) #branch 1
+    if len(tree) > 2:
+        atoms += alkyl(v4, destination - v1, tree[2]) #branch 2
+    if len(tree) > 3:
+        atoms += alkyl(v5, destination - v1, tree[3]) #branch 1
+    #untranslation
+    atoms = [(atom[0], atom[1] + v1) for atom in atoms]
+    
     return atoms
 
 
 def test():
-    v1 = np.array([1.0, 0.0, 0.0])
-    v2 = np.array([0.0, 1.0, 0.0])
-    atoms = alkyl(v1, v2, backbone=[["Ma",[],[]],["Mb",[["Mf",[],[]]],[]],["Mc",[["Me",[],[]]],[]],["Md",[],[]]])
+    logging.basicConfig(level=logging.DEBUG,
+                            format="%(asctime)s %(levelname)s %(message)s")
+    direction = np.array([1.0, 0.0, 0.0])    #must be a unit vector
+    destination = np.array([10.0, 10.0, 0.0])  #All the branches direct to this point.
+    atoms = alkyl(direction, destination, tree=["Ma",["Mb", "Mf", ["Mc", "Me", ["Md", ["A", ["B", ["C", ["D"]]]]]]]])
     # in yaplot format
+    print("t 0 0 0 +")
+    print("t",destination[0], destination[1], destination[2], "@")
     for atom in atoms:
         name, pos = atom
         print("t",pos[0], pos[1], pos[2], name)
--- genice/lattice.py
+++ genice/lattice.py
@@ -290,43 +290,50 @@ def butyl(cpos, root, cell, molname):
     """
     put a butyl group rooted at root toward cpos.
     """
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [], []], ["Mc", [], []], ["Md", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", ["Mc", "Md"]]])
+
+
+def pentyl(cpos, root, cell, molname):
+    """
+    put a butyl group rooted at root toward cpos.
+    """
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", ["Mc", ["Md", "Me"]]]])
 
 
 def propyl(cpos, root, cell, molname):
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [], []], ["Mc", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", "Mc"]])
 
 
 def ethyl(cpos, root, cell, molname):
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", "Mb"])
 
 
 def _2_2_dimethylpropyl(cpos, root, cell, molname):
     """
     2,2-dimethylpropyl group rooted at root toward cpos.
     """
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [["Md", [], []]], [["Me", [], []]]], ["Mc", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", "Mc", "Md", "Me"]])
 
 
 def _2_3_dimethylbutyl(cpos, root, cell, molname):
     """
     put a butyl group rooted at root toward cpos.
     """
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [["Mf", [], []]], []], ["Mc", [["Me", [], []]], []], ["Md", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", ["Mc", "Md", "Me"], "Mf"]])
 
 
 def _3_methylbutyl(cpos, root, cell, molname):
     """
     put a butyl group rooted at root toward cpos.
     """
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [], []], ["Mc", [["Me", [], []]], []], ["Md", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", ["Mc", "Md", "Me"]]])
 
 
 def _3_3_dimethylbutyl(cpos, root, cell, molname):
     """
     put a butyl group rooted at root toward cpos.
     """
-    return Alkyl(cpos, root, cell, molname, [["Ma", [], []], ["Mb", [], []], ["Mc", [["Me", [], []]], [["Mf", [], []]]], ["Md", [], []]])
+    return Alkyl(cpos, root, cell, molname, ["Ma", ["Mb", ["Mc", "Md", "Me", "Mf"]]])
 
 
 def Alkyl(cpos, root, cell, molname, backbone):
@@ -335,22 +342,14 @@ def Alkyl(cpos, root, cell, molname, backbone):
     """
     logger = logging.getLogger()
     # logger.info("  Put butyl at {0}".format(molname))
-    v1 = cpos - root
-    v1 -= np.floor(v1 + 0.5)
-    v1 = np.dot(v1, cell)
-    v1 /= np.linalg.norm(v1)
-    v2 = np.random.random(3)
-    v12 = np.dot(v1, v2)
-    v2 -= v1 * v12
-    v2 /= np.linalg.norm(v2)  # a random unit vector perpendicular to v1
-    logger.debug("  {0} {1} {2}".format(
-        np.dot(v1, v1), np.dot(v2, v2), np.dot(v1, v2)))
-    #v3 = np.cross(v1,v2)
+    v1abs = cpos - root
+    v1abs -= np.floor(v1abs + 0.5)
+    v1abs = np.dot(v1abs, cell)
+    v1 = v1abs / np.linalg.norm(v1abs)
+
     origin = np.dot(root, cell)
     CC = 0.154
-    c = cos(109.5 / 2 * pi / 180)
-    s = (1.0 - c**2)**0.5
-    rawatoms = alkyl.alkyl(v1, v2, backbone)
+    rawatoms = alkyl.alkyl(v1, v1abs*1.5/CC, backbone)
 
     atoms = []
     for i, atom in enumerate(rawatoms):
@@ -531,6 +530,7 @@ class Lattice():
         # experimental; there are many variation of semi-guest inclusion.
         self.groups_placer = {"Bu-": butyl,
                               "Butyl-": butyl,
+                              "Pentyl-": pentyl,
                               "Propyl-": propyl,
                               "2,2-dimethylpropyl-": _2_2_dimethylpropyl,
                               "2,3-dimethylbutyl-": _2_3_dimethylbutyl,
