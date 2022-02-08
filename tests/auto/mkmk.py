cd8c1c3d6a55f7041222c30d28aad31a59cffa5f
Masakazu Matsumoto <vitroid@gmail.com>
2022-02-08 18:21:39 +0900

    .

--- tests/auto/mkmk.py
+++ tests/auto/mkmk.py
@@ -1,7 +1,10 @@
 # test of tests
-
+import sys
 from genice2.plugin import scan
 import json
+
+testmode = (len(sys.argv) > 1 and sys.argv[1] == "test")
+
 
 result = scan("lattice")
 # preinstalled (system) plugins only
@@ -24,9 +27,14 @@ for ice in ices:
             options = ""
         product = f"{ice}_{i}.gro"
         target = f"{ice}{test}"
-        rules.append(f"{product}: ../../genice2/lattices/{ice}.py\n")
-        rules.append(f"\t../../genice.x {target} {options} > $@\n")
-        products.append(product)
+        if testmode:
+            rules.append(f"{product}.diff: {product} ../../genice2/lattices/{ice}.py\n")
+            rules.append(f"\t../../genice.x {target} {options} | diff - $< \n") #> $@.diff\n")
+            products.append(f"{product}.diff")
+        else:
+            rules.append(f"{product}: ../../genice2/lattices/{ice}.py\n")
+            rules.append(f"\t../../genice.x {target} {options} > $@\n")
+            products.append(product)
 
 print("all:",*products)
 print("".join(rules))
