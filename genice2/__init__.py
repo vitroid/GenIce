7c7da100811f6539701815709003a1e184e4ca55
vitroid <vitroid@gmail.com>
2024-03-29 08:15:24 +0900

    -

--- genice2/__init__.py
+++ genice2/__init__.py
@@ -1,7 +1,15 @@
 # -*- coding: utf-8 -*-
+
+# Set the same value in two places in pyproject.toml
+# See https://semver.org if you wander how to version it.
+
 __version__ = "2.2.5"
 
 # 1st: major version
 # 2nd: minor version (refactorization)
 # 3rd: minor changes (new plugin, etc.)
 # 4th: bug fix.
+
+# next bug fix will be 2.2.5.1
+# next version candidates will be 2.2.6-alpha.1
+# next version with some new features will be 2.2.6
