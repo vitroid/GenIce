833545e5d3a988caf8536f2604834d114ffa3718
vitroid <vitroid@gmail.com>
2025-05-06 00:43:49 +0900

    -

--- tests/auto/mkmk.py
+++ tests/auto/mkmk.py
@@ -1,5 +1,6 @@
 # test of tests
 import sys
+import random
 
 sys.path.append("../..")
 from genice2.plugin import scan
@@ -40,6 +41,33 @@ for ice in ices:
                 genice_options = test["options"]
             if "args" in test:
                 module_options = options_parser(test["args"])
+        # other available options:
+        # - noise: float
+        # - assess_cages: bool
+        # - depol: str
+        # - shift: tuple
+        # - rep: tuple
+        # - reshape: np.ndarray
+        # - signature: str
+        # - density: float
+        # - cations: dict
+        # - anions: dict
+        # - spot_guests: dict
+        # - spot_groups: dict
+        # - asis: bool
+
+        additional_options = [
+            "--reshape 1,1,1,1,-1,0,1,1,-2",
+            # "--assess_cages",
+            "--shift 0.5 0.4 0.3",
+            "--add_noise 0.01",
+            "--water 4site",
+            "--cation 0=Na --anion 2=Cl",
+            "--asis",
+        ]
+
+        genice_options += " " + " ".join(random.sample(additional_options, 3))
+
         product = f"{ice}_{i}.gro"
         if module_options != "":
             module_options = "[" + module_options + "]"
