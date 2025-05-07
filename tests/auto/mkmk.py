15db23708221b651bc9238d2cb76b87095237cf2
vitroid <vitroid@gmail.com>
2025-05-07 12:40:53 +0900

    Makefileとmkmk.pyを更新し、テスト用のルールを整理しました。Makefileでは新しいルールファイルを使用し、mkmk.pyでは生成物のリストを分けて表示するようにしました。

--- tests/auto/mkmk.py
+++ tests/auto/mkmk.py
@@ -1,5 +1,6 @@
 # test of tests
 import sys
+import random
 
 sys.path.append("../..")
 from genice2.plugin import scan
@@ -15,14 +16,13 @@ def options_parser(options):
 basicConfig(level=DEBUG)
 logger = getLogger()
 
-testmode = len(sys.argv) > 1 and sys.argv[1] == "test"
-
 result = scan("lattice")
 # preinstalled (system) plugins only
 ices = result["system"]
 tests = result["tests"]
 # print(tests)
-products = []
+products_prepare = []
+products_test = []
 rules = []
 for ice in ices:
     if ice in ("iceR",):
@@ -40,23 +40,51 @@ for ice in ices:
                 genice_options = test["options"]
             if "args" in test:
                 module_options = options_parser(test["args"])
+        # other available options:
+        # - noise: float
+        # - assess_cages: bool
+        # - depol: str
+        # - shift: tuple
+        # - rep: tuple
+        # - reshape: np.ndarray
+        # - signature: str
+        # - density: float
+        # - cations: dict
+        # - anions: dict
+        # - spot_guests: dict
+        # - spot_groups: dict
+        # - asis: bool
+
+        additional_options = [
+            "--reshape 1,1,1,1,-1,0,1,1,-2",
+            # "--assess_cages",
+            "--shift 0.5 0.4 0.3",
+            "--add_noise 0.01",
+            "--water 4site",
+            "--cation 0=Na --anion 2=Cl",
+            "--asis",
+        ]
+
+        genice_options += " " + " ".join(random.sample(additional_options, 3))
+
         product = f"{ice}_{i}.gro"
         if module_options != "":
             module_options = "[" + module_options + "]"
         target = f"{ice}{module_options}"
         logger.debug(f"Target: {target}")
-        if testmode:
-            rules.append(f"{product}.diff: {product} ../../genice2/lattices/{ice}.py\n")
-            rules.append(
-                f"\t$(GENICE) {target} {genice_options} | diff - $< \n"
-            )  # > $@.diff\n")
-            products.append(f"{product}.diff")
-        else:
-            rules.append(f"{product}: ../../genice2/lattices/{ice}.py\n")
-            rules.append(f"\t$(GENICE) {target} {genice_options} > $@\n")
-            products.append(product)
+        # if testmode:
+        rules.append(f"{product}.diff: {product} ../../genice2/lattices/{ice}.py\n")
+        rules.append(
+            f"\t$(GENICE) {target} {genice_options} | diff - $< \n"
+        )  # > $@.diff\n")
+        products_test.append(f"{product}.diff")
+        # else:
+        rules.append(f"{product}: ../../genice2/lattices/{ice}.py\n")
+        rules.append(f"\t$(GENICE) {target} {genice_options} > $@\n")
+        products_prepare.append(product)
 
 print("GENICE=../../genice.x")
 # print("GENICE=genice2")
-print("all:", *products)
+print("prepare:", *products_prepare)
+print("test:", *products_test)
 print("".join(rules))
