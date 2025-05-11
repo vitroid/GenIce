b5d303aef094046e3c4c7b708712573acb4ac2fb
vitroid <vitroid@gmail.com>
2025-05-11 19:17:16 +0900

    HS1.pyに新しいオプションを追加し、mkmk.pyでのテスト生成ロジックを改善しました。これにより、テストのオプション処理が強化され、フォーマットの選択がランダム化されました。

--- genice2/lattices/HS1.py
+++ genice2/lattices/HS1.py
@@ -8,6 +8,24 @@
 import genice2.lattices
 from genice2.cell import cellvectors
 
+desc = {
+    "ref": {"HS1": "Kosyakov and Polyanskaya 1999"},
+    "usage": "No options available.",
+    "brief": "Hydrogen-disordered ice Ih.",
+    "test": (
+        {"options": "--assess_cages"},
+        {"options": "--guest 12=me"},  # fill type 12 cage with methane molecules.
+        {"options": "--spot_guest 12=me"},
+        {
+            "options": (
+                "--anion 1=Br --cation 3=N "
+                "--depol=optimal "
+                "-H 9=Bu:3 -H 11=Bu:3 -H 13=Bu:3 -H 7=Bu:3"
+            )
+        },
+    ),
+}
+
 
 class Lattice(genice2.lattices.Lattice):
     def __init__(self):
@@ -121,6 +139,4 @@ class Lattice(genice2.lattices.Lattice):
         15    0.6667    0.0000    1.0000
         """
 
-        self.cell = cellvectors(a=20.0715,
-                                b=11.588333333,
-                                c=11.754333333)
+        self.cell = cellvectors(a=20.0715, b=11.588333333, c=11.754333333)
--- tests/auto/mkmk.py
+++ tests/auto/mkmk.py
@@ -5,7 +5,7 @@ import glob
 import os
 
 sys.path.append("../..")
-from genice2.plugin import scan
+from genice2 import plugin
 from logging import getLogger, basicConfig, DEBUG, INFO
 
 
@@ -18,14 +18,56 @@ def options_parser(options):
 basicConfig(level=DEBUG)
 logger = getLogger()
 
-result = scan("lattice")
-# preinstalled (system) plugins only
-ices = result["system"]
-tests = result["tests"]
 # print(tests)
-products_prepare = []
-products_test = []
-rules = []
+
+
+# other available options:
+# - noise: float
+# - assess_cages: bool
+# - depol: str
+## - shift: tuple
+# - rep: tuple
+## - reshape: np.ndarray
+# - signature: str
+# - density: float
+# - cations: dict
+# - anions: dict
+# - spot_guests: dict
+# - spot_groups: dict
+# - asis: bool
+
+
+def make_test(ice: str, tests: dict, format: str, formatter_path: str):
+    if ice == "HS1":
+        logger.info(
+            f"Make tests of {ice} tests {tests} in {format} via {formatter_path}"
+        )
+
+    for i, test in enumerate(tests):
+        product = f"{ice}_{i}.output"
+
+        genice_options = ""
+        module_options = ""
+
+        logger.debug(f"{ice} {test}")
+        if "options" in test:
+            genice_options = test["options"]
+        if "args" in test:
+            module_options = options_parser(test["args"])
+
+        genice_options += " " + " ".join(random.sample(additional_options, 3))
+        genice_options += f" -f {format} "
+        if module_options != "":
+            module_options = "[" + module_options + "]"
+        target = f"{ice}{module_options}"
+        logger.debug(f"Target: {target}")
+        # if testmode:
+        rule = f"{product}.diff: {product} ../../genice2/lattices/{ice}.py {formatter_path}\n"
+        rule += f"\t$(GENICE) {target} {genice_options} | diff - $< && touch $@\n"
+        rule += f"{product}: ../../genice2/lattices/{ice}.py  {formatter_path}\n"
+        rule += f"\t$(GENICE) {target} {genice_options} > $@\n"
+        yield product, rule
+
 
 additional_options = [
     "--reshape 1,1,1,1,-1,0,1,1,-2",
@@ -36,71 +78,48 @@ additional_options = [
     "--cation 0=Na --anion 2=Cl",
     # "--asis",
 ]
-formatter_paths = {}
-for filepath in glob.glob("../../genice2/formats/*.py"):
-    if not os.path.islink(filepath):
-        formatter_prefix = os.path.basename(filepath).split(".")[0]
-        if formatter_prefix in ("raw", "null", "__init__"):
-            continue
-        formatter_paths[f" -f {formatter_prefix}"] = filepath
+
+
+def formatter_list():
+    formatter_paths = {}
+    for filepath in glob.glob("../../genice2/formats/*.py"):
+        if not os.path.islink(filepath):
+            formatter_prefix = os.path.basename(filepath).split(".")[0]
+            if formatter_prefix in ("raw", "null", "__init__"):
+                continue
+            formatter_paths[f" -f {formatter_prefix}"] = filepath
+    return formatter_paths
+
+
+result = plugin.scan("lattice")
+# preinstalled (system) plugins only
+ices = result["system"]
+tests = result["tests"]
+formatters = formatter_list()
+
+# print(tests)
+# sys.exit(0)
+
+products_prepare = []
+products_test = []
+rules = ""
+
 
 for ice in ices:
     if ice in ("iceR",):
         continue
     if ice not in tests:
         tests[ice] = ("",)
+    format = random.choice(list(formatters.keys()))
 
-    for i, test in enumerate(tests[ice]):
-        genice_options = ""
-        module_options = ""
-
-        if type(test) is dict:
-            logger.debug(f"{ice} {test}")
-            if "options" in test:
-                genice_options = test["options"]
-            if "args" in test:
-                module_options = options_parser(test["args"])
-        # other available options:
-        # - noise: float
-        # - assess_cages: bool
-        # - depol: str
-        # - shift: tuple
-        # - rep: tuple
-        # - reshape: np.ndarray
-        # - signature: str
-        # - density: float
-        # - cations: dict
-        # - anions: dict
-        # - spot_guests: dict
-        # - spot_groups: dict
-        # - asis: bool
-
-        genice_options += " " + " ".join(random.sample(additional_options, 3))
-        format_option = random.choice(list(formatter_paths.keys()))
-        genice_options += format_option
-        product = f"{ice}_{i}.output"
-        if module_options != "":
-            module_options = "[" + module_options + "]"
-        target = f"{ice}{module_options}"
-        logger.debug(f"Target: {target}")
-        # if testmode:
-        rules.append(
-            f"{product}.diff: {product} ../../genice2/lattices/{ice}.py {formatter_paths[format_option]}\n"
-        )
-        rules.append(
-            f"\t$(GENICE) {target} {genice_options} | diff - $< && touch $@\n"
-        )  # > $@.diff\n")
-        products_test.append(f"{product}.diff")
-        # else:
-        rules.append(
-            f"{product}: ../../genice2/lattices/{ice}.py  {formatter_paths[format_option]}\n"
-        )
-        rules.append(f"\t$(GENICE) {target} {genice_options} > $@\n")
+    for product, rule in make_test(ice, tests[ice], format, formatters[format]):
         products_prepare.append(product)
+        products_test.append(product)
+        rules += rule
 
 print("GENICE=../../genice.x")
 # print("GENICE=genice2")
 print("TARGETS=", *products_prepare)
 print("prepare: $(TARGETS)")
 print("test: $(patsubst %, %.diff, $(TARGETS))")
-print("".join(rules))
+print(rules)
